86804cb2e0b46cb315a9609f8dbe886c3d4c8397
p000ison <maximilian.ammann@googlemail.com>
2014-12-05 16:05:55 +0100

    Initial commit

---
+++ src/main/java/org/societies/bridge/sponge/SpongeWorld.java
@@ -0,0 +1,32 @@
+package org.societies.bridge.sponge;
+
+import org.societies.bridge.ItemStack;
+import org.societies.bridge.Location;
+import org.societies.bridge.World;
+
+import java.util.UUID;
+
+/**
+ * Represents a SpongeWorld
+ */
+public class SpongeWorld implements World {
+
+    private final org.spongepowered.api.world.World world;
+
+    public SpongeWorld(org.spongepowered.api.world.World world) {this.world = world;}
+
+    @Override
+    public void dropItem(Location location, ItemStack itemStack) {
+
+    }
+
+    @Override
+    public UUID getUUID() {
+        return world.getUniqueID();
+    }
+
+    @Override
+    public String getName() {
+        return world.getName();
+    }
+}
